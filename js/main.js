ec96f14744dd964f71b8f35697a02debb80ee9f5
Maksim <akiam@mail.ru>
2016-01-31 19:27:19 +0300

    seems good

--- js/main.js
+++ js/main.js
@@ -1,39 +1,57 @@
 $(document).ready(function () {
+
+    $('.overlay').css({'height': $(document).height()+'px'});
+
+    $('.page').parallax({
+        imageSrc: './img/bg1.jpg',
+        iosFix: true,
+        androidFix: true
+    });
     //some logic for slide down food menu
 
-    if($(document).width()<992){
+    if($(document).width()<768){
         $('.food-inner').on('click', function () {
             if($('.food-inner-menu').is(':hidden')){
-                $('.food-inner-menu').slideDown();
+                $('.food-inner-menu').append('<div class="close-btn"><i class="fa fa-close"></i><p>свернуть</p></div>').slideDown();
             }else{
-                $('.food-inner-menu').slideUp();
+                $('.food-inner-menu').slideUp(function () {
+                    $('.close-btn').remove();
+                });
             }
         });
         $('.drinks-inner').on('click', function () {
             if($('.drinks-inner-menu').is(':hidden')){
-                $('.drinks-inner-menu').slideDown();
+                $('.drinks-inner-menu').append('<div class="close-btn"><i class="fa fa-close"></i><p>свернуть</p></div>').slideDown();
             }else{
-                $('.drinks-inner-menu').slideUp();
+                $('.drinks-inner-menu').slideUp(function () {
+                    $('.close-btn').remove();
+                });
             }
         });
     }else{
         $('.food-inner').on('click', function () {
             if($('.food-inner-menu').is(':hidden')){
-                $('.food-inner-menu').insertBefore('#contacts').slideDown();
+                $('.food-inner-menu').append('<div class="close-btn"><i class="fa fa-close"></i><p>свернуть</p></div>').insertBefore('#contacts').slideDown();
             }else{
-                $('.food-inner-menu').insertBefore('#contacts').slideUp();
+                $('.food-inner-menu').slideUp(function () {
+                    $('.close-btn').remove();
+                });
             }
         });
         $('.drinks-inner').on('click', function () {
             if($('.drinks-inner-menu').is(':hidden')){
-                $('.drinks-inner-menu').insertBefore('#contacts').slideDown();
+                $('.drinks-inner-menu').append('<div class="close-btn"><i class="fa fa-close"></i><p>свернуть</p></div>').insertBefore('#contacts').slideDown();
             }else{
-                $('.drinks-inner-menu').insertBefore('#contacts').slideUp();
+                $('.drinks-inner-menu').slideUp(function(){
+                    $('.close-btn').remove();
+                });
             }
         });
     }
-    $('.fa-close').on('click', function () {
+
+    $(document).on('click', '.close-btn', function () {
         var parElem=$(this).parent();
         $(parElem).slideUp();
+        $(this).remove();
     });
 });
